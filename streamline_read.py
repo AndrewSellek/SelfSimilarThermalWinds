22df0115daf880d22ea30c1ff8ff5ec0de5e45a7
AndrewSellek <ads79@cam.ac.uk>
2021-02-26 00:46:19 +0000

    Added table reader and streamline reader

---
+++ streamline_read.py
@@ -0,0 +1,176 @@
+"""""""""
+Import Packages and Setup Python Environment
+"""""""""
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+
+b_def   = 1.5
+t_def   = 0.0
+phi_def = 0.0
+chi_def = 90
+
+"""""""""
+Define Streamline object class
+"""""""""
+class Streamline(object):
+
+    # Initialise
+    def __init__(self, streamlineData, header_dict):
+        self._x = streamlineData['x']
+        self._y = streamlineData['y']
+        self._u = streamlineData['u']
+        self._dxdy = streamlineData['dx']
+
+        try:
+            self._Mach_b = header_dict['M_b']
+        except:
+            self._Mach_b = None
+
+        try:
+            self._b = header_dict['b']
+        except:
+            self._b = b_def
+
+        try:
+            self._t = header_dict['t']
+        except:
+            self._t = t_def
+
+        try:
+            self._phi_b = header_dict['phi_b']
+        except:
+            self._phi = phi_def
+
+        try:
+            self._chi_b = header_dict['chi_b']
+        except:
+            self._chi_b = chi_def/180
+
+    # Spatial coordinates
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def r(self):
+        return np.sqrt(self._x**2+self._y**2)
+
+    # Angles
+    @property
+    def phi(self):
+        return np.arctan2(self._y,self._x)
+
+    @property
+    def theta(self):
+        return np.arctan2(1,self._dxdy)
+
+    @property
+    def chi(self):
+        return self.theta - self.phi
+
+    # Velocity and components
+    @property
+    def u(self):
+        return self._u
+
+    @property
+    def ux(self):
+        return self._u * np.cos(self.theta)
+
+    @property
+    def uy(self):
+        return self._u * np.sin(self.theta)
+
+    @property
+    def ur(self):
+        return self._u * np.cos(self.chi)
+
+    @property
+    def utheta(self):
+        return -self._u * np.sin(self.chi)
+
+    # Mach number
+    @property
+    def Mach_b(self):
+        return self._Mach_b
+
+    # Model properties
+    @property
+    def b(self):
+        return self._b
+
+    @property
+    def t(self):
+        return self._t
+
+    @property
+    def phi_b(self):
+        return self._phi_b
+
+    @property
+    def chi_b(self):
+        return self._chi_b
+
+def read_streamline(dataFile):
+    # Read file
+    try:
+        streamlineData = np.genfromtxt(dataFile, names=True, skip_header=1, comments='#')
+        with open(dataFile) as f:
+            header_dict = {}
+            header = f.readline().strip()
+            print("Analytic streamline found: {}\n".format(dataFile), header)
+            header_split = header.split(",")
+            for i in range(len(header_split)):
+                if i ==0:
+                    header_split[i] = header_split[i].split(" ")[-1]
+                header_split[i] = header_split[i].strip()
+                header_split[i] = header_split[i].split("=")
+                try:
+                    header_dict[header_split[i][0]]=float(header_split[i][1])
+                except ValueError:
+                    header_dict[header_split[i][0]]=float(header_split[i][1].split("p")[0])
+    except ValueError:
+        # Catch old versions that don't have a header
+        streamlineData = np.genfromtxt(dataFile, names=True, skip_header=0, comments='#')
+        print("Analytic streamline found:")
+        header_dict = None
+    except OSError:
+        print("File ", dataFile, " could not be found")
+        return None
+
+    # Store as streamline object
+    stream = Streamline(streamlineData, header_dict)
+
+    return stream
+
+def main():
+    # Test script to make a basic plot of the streamline on the R-z plane
+    """""""""
+    Argument Setup
+    """""""""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", "-f", nargs='+', type=str, default=None, help='Provide *.dat file(s) for analytic streamline.')
+    args = parser.parse_args()
+
+    # Plot the streamlines
+    plt.figure()
+    for dataFile in args.file:
+        stream = read_streamline(dataFile)
+        if stream:
+            plt.plot(stream.x, stream.y, label='$\mathcal{{M}} = {:03.3f}$'.format(stream.Mach_b))
+
+    # Decorate and display plot
+    plt.xlim([0,10])
+    plt.ylim([0,10])
+    plt.xlabel('$\\tilde{R}$')
+    plt.ylabel('$\\tilde{z}$')
+    plt.legend()
+    plt.show()
+
+if __name__=="__main__":
+    main()
---
+++ table_read.py
@@ -0,0 +1,108 @@
+"""""""""
+Import Packages and Setup Python Environment
+"""""""""
+import numpy as np
+import argparse
+
+b_def   = 1.5
+t_def   = 0.0
+k_def   = 's'
+phi_def = 0.0
+chi_def = 90
+
+def read_Mach_table(tableFile, choose_k=k_def):
+    # Reads a .dat file containing the maximum launch Mach numbers for a range of wind geometries and density/temperature profiles and stores in a dictionary.
+    # By default returns only for spherical temperature profiles. For cylindrical call with choose_k='c'.
+    table_data = np.genfromtxt(tableFile, names=True, skip_header=0, comments='#')
+    Mach_data = table_data['M_b']
+
+    # Extract density slopes b
+    try:
+        all_b = table_data['b']
+    except ValueError:
+        all_b = np.full_like(Mach_data, b_def)
+    unique_b = np.unique(all_b)
+
+    # Extract temperature slopes t
+    try:
+        all_t = table_data['t']
+    except ValueError:
+        all_t= np.full_like(Mach_data, t_def)
+    unique_t = np.unique(all_t)
+
+    # Extract temperature keys k
+    try:
+        all_k = table_data['T_key']
+    except ValueError:
+        all_k= np.full_like(Mach_data, k_def)
+    unique_k = np.unique(all_k)
+
+    # Extract base angles phi_b
+    try:
+        all_phi = table_data['phi_b']
+    except ValueError:
+        all_phi = np.full_like(Mach_data, phi_def)
+    unique_phi = np.unique(all_phi)
+
+    # Extract launch angles chi_b
+    try:
+        all_chi = table_data['chi_bpi']
+    except ValueError:
+        all_chi = np.full_like(Mach_data, chi_def/180)
+    unique_chi = np.unique(all_chi)
+
+    # Create dictionary
+    Mach_table = {}
+    for potential_b in unique_b:
+        Mach_table[potential_b] = {}
+        for potential_t in unique_t:
+            Mach_table[potential_b][potential_t] = {}
+            for potential_phi in unique_phi:
+                Mach_table[potential_b][potential_t][potential_phi] = {}
+                for potential_chi in unique_chi:
+                    Mach_table[potential_b][potential_t][potential_phi][potential_chi] = np.inf # Initialise to infinity so all true Mach numbers are less
+
+    # Populate dictionary with lowest Mach fitting those parameters
+    for b, t, k, phi_b, chi_b, Mach_b in zip(all_b, all_t, all_k, all_phi, all_chi, Mach_data):
+        if Mach_b < Mach_table[b][t][phi_b][chi_b] and k==choose_k:
+            # If less than existing record, replace it
+            Mach_table[b][t][phi_b][chi_b] = Mach_b
+
+    return Mach_table
+
+def search_Mach_table(Mach_table, b, t, phi_b, chi_b):
+    try:
+        return Mach_table[b][t][phi_b][chi_b]
+    except:
+        return np.nan
+
+def main():
+    """""""""
+    Argument Setup
+    """""""""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", "-f", type=str, default='launch_Mach.dat', help='File containing launch Mach numbers')
+    parser.add_argument("--b", "-b", type=float, nargs='+', default=[b_def], help='Density Profile Index'    )
+    parser.add_argument("--t", "-t", type=float, nargs='+', default=[t_def], help='Temperature Profile Index')
+    parser.add_argument("--phi_b", "-p", type=float, nargs='+', default=[phi_def],  help='Angle of the wind base (degrees) [Default: 0]' )
+    parser.add_argument("--chi_b", "-c", type=float, nargs='+', default=[chi_def], help='Angle of the wind base (degrees) [Default: 90]')
+    args = parser.parse_args()
+
+    lengths = [len(args.b), len(args.t), len(args.phi_b), len(args.chi_b)]
+    if not all(l==lengths[0] for l in lengths):
+        raise AssertionError("The lists of parameters provided do not all have the same lengths.")
+
+    Mach_table = read_Mach_table(args.file)
+
+    for b, t, phi_b, chi_b in zip(args.b, args.t, args.phi_b, args.chi_b):
+        Mach_b = search_Mach_table(Mach_table, b, t, phi_b, chi_b/180)
+        if not np.isnan(Mach_b):
+            print("b = {:3.2f}, t = {:3.2f}, phi_b = {:2.0f}, chi_b = {:3.3f}: \t M_b = {:3.3f}".format(b, t, phi_b, chi_b, Mach_b))
+        else:
+            print("Mach number could not be found for b = {:3.2f}, t = {:3.2f}, phi_b = {:2.0f}, chi_b = {:3.3f}".format(b, t, phi_b, chi_b))
+
+    return 1
+
+if __name__=="__main__":
+    main()
+
